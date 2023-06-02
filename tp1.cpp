cc09ad763efcae61b764a284ea89e6da7c74ab93
LucasAponte <127443180+LucasAponte@users.noreply.github.com>
2023-06-02 00:33:33 -0300

    Add files via upload

---
+++ tp1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int
+    double pi = 0;
+    while (abs(pi-3.141592)>0.0000001)
+    {
+        double denominador = 1.0 + 2.0 * aux;
+    }
+    
+    cout << "hola";
+    return 0;
+}
